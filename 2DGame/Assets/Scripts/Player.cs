0fbd7f92afd9243a6a6ed5d6c19e501e9bb43c18
107051358 <72062978+107051358@users.noreply.github.com>
2020-11-03 16:02:52 +0800

    第七周選單管理

--- 2DGame/Assets/Scripts/Player.cs
+++ 2DGame/Assets/Scripts/Player.cs
@@ -2,11 +2,12 @@
 
 public class Player : MonoBehaviour
 {
+    public GameObject final;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print("碰到傳送門了");
-
+        final.SetActive(true);
     }
 
 }
