8ce2ef84f892d2343f5d5ba80345d65dfbc0f412
107051358 <72062978+107051358@users.noreply.github.com>
2020-11-10 16:01:24 +0800

    第八周2D結束

--- 2DGame/Assets/Scripts/Player.cs
+++ 2DGame/Assets/Scripts/Player.cs
@@ -1,13 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public GameObject final;
 
+    public Text textCount;
+
+    public int count;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("碰到傳送門了");
-        final.SetActive(true);
+        if (collision.name == "傳送門")
+        {
+            final.SetActive(true);
+        }
+
+
+        if (collision.tag == "櫻桃")
+        {
+            Destroy(collision.gameObject);
+
+            count++;
+
+            textCount.text = "櫻桃數量:" + count;
+
+        }
     }
 
 }
